10cc45c087b2889e75156ef9f2cd03d562fb022f
zhengchanglin <yh.lvdousha@gmail.com>
2017-10-11 17:21:26 +0800

    http方式访问ES

---
+++ src/main/java/com/stone/es/http/ESAddress.java
@@ -0,0 +1,7 @@
+package com.stone.es.http;
+
+public class ESAddress {
+
+	public final static String CAT_INDICES = "http://localhost:9200/_cat/indices?v"; //获取全部索引的信息，包括 健康状态，分片数量，记录数量，占用空间
+	
+}
---
+++ src/main/java/com/stone/es/http/ESHttp.java
@@ -0,0 +1,21 @@
+package com.stone.es.http;
+
+import org.apache.log4j.Logger;
+
+public class ESHttp {
+
+	private Logger log = Logger.getLogger(this.getClass());
+	public static void main(String[] args) {
+		ESHttp http = new ESHttp();
+		http.getIndices();
+	}
+	
+	public void getIndices(){
+		String result = HttpClientUtil.get("http://localhost:9200/_cat/indices?v");
+//		log.info(result);
+		System.out.println(result);
+	}
+	
+	
+	
+}
