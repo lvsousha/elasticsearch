5da21809e206bc11207277a2ca6910f9c459c07d
zhengchanglin <yh.lvdousha@gmail.com>
2017-10-17 17:54:33 +0800

    根据shield进行连接

--- src/main/java/com/stone/es/ESClient.java
+++ src/main/java/com/stone/es/ESClient.java
@@ -1,19 +1,21 @@
 package com.stone.es;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.shield.ShieldPlugin;
 
 import com.stone.utils.PropertiesUtils;
 
 public class ESClient {
 	
 	public static void main(String[] args) throws Exception {
-		Client client = ESClient.createClientBySetting();
-		
+//		Client client = ESClient.createClientBySetting();
+		Client client = ESClient.createClientShield();
 //		XContentBuilder  builder  =  XContentFactory.jsonBuilder()
 //		        .startObject()
 //		                .field("user",  "kimchy")
@@ -47,6 +49,32 @@ public class ESClient {
 //                					.put("client.transport.nodes_sampler_interval",  "1000")	//定时对列出和连接的节点进行抽样/ ping操作。 默认为5秒。
                 					.build();
 		Client client  =  TransportClient.builder()
+										.settings(settings)
+										.build()
+										.addTransportAddress(
+												new InetSocketTransportAddress(
+														InetAddress.getByName(url.split(":")[0]), 
+														Integer.parseInt(url.split(":")[1]))
+										);
+		return client;
+	}
+	
+	public static Client createClientShield() throws NumberFormatException, UnknownHostException{
+		String clusterName = PropertiesUtils.getString("cluster.name");
+//		String nodeNamee = PropertiesUtils.getString("node.name");
+		String url = PropertiesUtils.getString("es.url");
+		String shieldUser = PropertiesUtils.getString("shield.user");
+		Settings settings  =  Settings.settingsBuilder()
+                					.put("cluster.name",  clusterName)
+//                					.put("node.name",  nodeNamee)
+                					.put("shield.user", shieldUser)
+//                					.put("client.transport.sniff",  "true")
+//                					.put("client.transport.ignore_cluster_name",  "true")		//设置为true可忽略连接的节点的集群名称验证
+//                					.put("client.transport.ping_timeout",  "1000")	//等待来自节点的ping响应的时间。 默认为5秒。
+//                					.put("client.transport.nodes_sampler_interval",  "1000")	//定时对列出和连接的节点进行抽样/ ping操作。 默认为5秒。
+                					.build();
+		Client client  =  TransportClient.builder()
+										.addPlugin(ShieldPlugin.class)
 										.settings(settings)
 										.build()
 										.addTransportAddress(
