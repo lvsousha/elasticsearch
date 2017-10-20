2fc7a2e12ed8ea5dc9afb31048e5953419406375
zhengchanglin <yh.lvdousha@gmail.com>
2017-10-21 00:02:21 +0800

    分析接口，查询接口，解释接口

--- src/main/java/com/stone/es/http/ESHttp.java
+++ src/main/java/com/stone/es/http/ESHttp.java
@@ -4,10 +4,10 @@ import org.apache.log4j.Logger;
 
 public class ESHttp {
 
-	private Logger log = Logger.getLogger(this.getClass());
+	private static Logger log = Logger.getRootLogger();
 	public static void main(String[] args) {
 		ESHttp http = new ESHttp();
-		http.bulkRequest();
+		http.analyze();
 	}
 	
 	public void getIndices(){
@@ -19,6 +19,24 @@ public class ESHttp {
 	public void bulkRequest(){
 		String result = HttpClientUtil.post("http://39.106.37.249:9200/my_store/products/_bulk",
 				"{ \"index\": { \"_id\": 9 }}\n{ \"price\" : 10, \"productID\" : \"XHDK-A-1293-#fJ3\" }\n");
+		log.info(result);
+		System.out.println(result);
+	}
+	
+	public void analyze(){
+		String result = HttpClientUtil.post("http://localhost:9287/flfg/_analyze", "{\"field\": \"neirong\",\"text\": \"中华人民共和国\"}");
+		log.info(result);
+		System.out.println(result);
+	}
+	
+	public static void explain(String message){
+		String result = HttpClientUtil.post("http://localhost:9287/flfg/flfg/_validate/query?explain", message);
+		log.info(result);
+		System.out.println(result);
+	}
+	
+	public static void search(String message){
+		String result = HttpClientUtil.post("http://localhost:9287/flfg/flfg/_search", message);
 		log.info(result);
 		System.out.println(result);
 	}
