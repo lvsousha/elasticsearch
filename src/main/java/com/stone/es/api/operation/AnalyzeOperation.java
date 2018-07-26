407aa51ee6e216a521dc78fd71861bc5b2bab8ac
郑昌林 <yh.lvdousha@gmail.com>
2018-07-26 16:36:50 +0800

    添加注释

--- src/main/java/com/stone/es/api/operation/AnalyzeOperation.java
+++ src/main/java/com/stone/es/api/operation/AnalyzeOperation.java
@@ -1,6 +1,7 @@
 package com.stone.es.api.operation;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.http.HttpEntity;
@@ -16,11 +17,14 @@ public class AnalyzeOperation {
 
   public static void main(String[] args) throws IOException {
     // TODO Auto-generated method stub
+    System.out.println(Calendar.getInstance().getTimeInMillis());
     RestClient client = ESRestClient.getDefaultRestClientSecurity();
+    System.out.println(Calendar.getInstance().getTimeInMillis());
     BasicHeader header = new BasicHeader("Content-type","application/json");
     JSONObject object = new JSONObject();
     Map<String, String> params = new HashMap<>();
     object.put("analyzer", "ik_max_word");
+//    object.put("analyzer", "ik_smart");
     object.put("text", "最高人民法院、最高人民检察院关于办理危害生产安全刑事");
     HttpEntity entity = new NStringEntity(object.toJSONString(), "UTF-8");
 //    Response response = client.performRequest("GET", "laws/_analyze",params);
--- src/test/java/com/stone/es/api/CatApiTest.java
+++ src/test/java/com/stone/es/api/CatApiTest.java
@@ -9,10 +9,15 @@ public class CatApiTest {
   public static void main(String[] args) throws IOException {
     // TODO Auto-generated method stub
     RestClient client = ESRestClient.getDefaultRestClientSecurity();
+    System.out.println("------------索引健康状态------------------");
     CatApis.getHealth(client);
+    System.out.println("------------别名------------------");
     CatApis.getAlias(client);
+    System.out.println("------------数量------------------");
     CatApis.getCount(client);
+    System.out.println("------------索引状态------------------");
     CatApis.getIndices(client);
+    System.out.println("------------插件状态------------------");
     CatApis.getPlugins(client);
     ClusterApis.getClusterHealth(client);
     client.close();
